2a39be7bf9ef8dc5408644877bf645e3ffe56a17
maksim_murzintsev <murrrzintsev@mail.ru>
2021-12-11 00:57:22 +0300

    DONE SUKA BLYAT

--- app/controllers/laba8_controller.rb
+++ app/controllers/laba8_controller.rb
@@ -4,18 +4,50 @@ class Laba8Controller < ApplicationController
 
   def view
     @received = params[:v1]
-    @received_index = params[:k]
-    @result = !@received || @received.empty? ? 'Unknown!':calculate(params[:v1], params[:k])
+    @result = calculate(params[:v1])
+    # puts @result
   end
 end
 
-def calculate(massive, k)
-  $sum = 0
-  mas = massive.split(/[, ;]/).map(&:to_i).each do |el|
-    if (el.to_f%3 == 0)
-      $sum=$sum+el
-    end
+def calculate(massive)
+  kol = 0;
+
+  def stepen(x)
+      a = 1
+      while a<=x
+          a = a * 5
+          if a == x then
+              return true
+          end
+      end 
+      return false;
   end
-  mas[k.to_i-1]=$sum
-  return mas
+
+  segment = Array[];
+  segmentsHash = Hash.new => {};
+  j = 0;
+  max = Array[];
+  mas = massive.split(/[, ;]/).map(&:to_i)
+  mas.each_index do |i|
+      if (mas[i+1]!=nil && stepen(mas[i]) && stepen(mas[i+1]))
+          segment.push(mas[i])
+      elsif (mas[i-1]!=nil && stepen(mas[i]) && stepen(mas[i-1]))
+          segment.push(mas[i])
+      end
+      if ((mas[i+1]!=nil && !stepen(mas[i+1]) && !segment.empty?) || (mas[i+1] == nil && !segment.empty?))
+          segmentsHash[j] = segment;
+          j = j+1;
+          kol = kol+1;
+          if (segment.length > max.length)
+              max = segment
+          end
+          segment = []
+      end
+  end
+  if (kol>0)
+    result = [segmentsHash, kol, max]
+  else
+    result = 0
+  end
+  return result
 end
