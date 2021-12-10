7aab15c95857cab0a36eac19b373197bbf0c7b67
maksim_murzintsev <murrrzintsev@mail.ru>
2021-12-10 20:21:15 +0300

    Copy from RK2

--- app/controllers/laba8_controller.rb
+++ app/controllers/laba8_controller.rb
@@ -3,5 +3,19 @@ class Laba8Controller < ApplicationController
   end
 
   def view
+    @received = params[:v1]
+    @received_index = params[:k]
+    @result = !@received || @received.empty? ? 'Unknown!':calculate(params[:v1], params[:k])
   end
+end
+
+def calculate(massive, k)
+  $sum = 0
+  mas = massive.split(/[, ;]/).map(&:to_i).each do |el|
+    if (el.to_f%3 == 0)
+      $sum=$sum+el
+    end
+  end
+  mas[k.to_i-1]=$sum
+  return mas
 end
